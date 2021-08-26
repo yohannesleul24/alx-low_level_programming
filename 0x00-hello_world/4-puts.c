d9eb749b2542dd9b79bb56fe148e760f81138ac6
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-08-26 12:41:53 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry point
- * @void: no arguments
- * Description: the program starts and ends here.
- * Return: value 0 to exit
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+  return (0); }
