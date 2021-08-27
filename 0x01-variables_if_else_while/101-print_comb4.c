96da0f0879d27c9c06eb5eea33c963c88e75fe4a
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-08-27 13:49:11 -0700

    twelve

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
+
 /**
- * main - Prints 3 combination of numbers
- *
- * Return: Always (Success)
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
-{ int c, i, k;
-for (c = '0'; c <= '9'; c++)
 {
-for (i = '0'; i <= '9'; i++)
+int n, m, l;
+for (n = 48; n < 58; n++)
 {
-for (k = '0'; k <= '9'; k++)
+for (m = 49; m < 58; m++)
+
+for (l = 50; l < 58; l++)
+{
+if (l > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
+{
+putchar(',');
+putchar(' ');
 }
-if (c < i && i < k{ putchar(c);
-putchar(i);
-putchar(k);
-if (c != '7')
-{ putchar(',');
-putchar(' '); }}}}}
+}
+}
+}
+}
 putchar('\n');
-return (0); }
+return (0);
+}
