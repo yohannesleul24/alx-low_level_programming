57f7579589fda21a73f7fa1001cbaa8f068d0d7a
root <yohannesleul24@gmail.comgit config --global user.name yohannesgit comfig --global user.email yohannesleul24@gmail.com>
2021-11-25 09:55:37 -0800

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,24 @@
 #include <stdio.h>
 
 /**
-* print_dlistint - print all the elements of a dlistint_t list
-* @h: pointer to the start of the linked list
-*
-* Return: number of nodes
-*/
+ * print_dlistint - prints a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t i;
+size_t nodes = 0;
 
-for (i = 0; h != NULL; i++)
+if (!h)
+return (0);
+
+while (h)
 {
 printf("%d\n", h->n);
 h = h->next;
+nodes++;
 }
-return (i);
+
+return (nodes);
 }
